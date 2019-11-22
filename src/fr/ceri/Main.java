28ba6c55c6e9e5823d9ff75591dc4b711ba8f4e1
Loic MENGUY <loicm97@gmail.com>
2019-11-22 10:00:06 +0100

    on s'en tamponne
    oui

--- src/fr/ceri/Main.java
+++ src/fr/ceri/Main.java
@@ -1,24 +1,40 @@
 package fr.ceri;
+import org.json.JSONObject;
+import org.json.XML;
 
 import java.io.*;
 import java.util.Arrays;
 
 public class Main
 {
-    public static void main(String[] args) throws FileNotFoundException
-    {
-        try
-        {
-            System.out.println("Tweets");
 
-            LexiqueCreator lexLuthor = new LexiqueCreator();
-            lexLuthor.writeLexiqueToFile();
-            lexLuthor.transformTweets(new File("src/fr/ceri/data/task1-train.csv"));
-        }
-        catch (Exception ex)
-        {
-            System.out.println("Exception caught:" + ex.getMessage());
-            System.out.println("Stack trace:" + Arrays.toString(ex.getStackTrace()));
-        }
+    public static void main(String[] args) throws IOException
+    {
+        String testFile = new File("src/fr/ceri/data/unlabeled.xml").toString();
+        System.out.println(testFile);
+
+        FileInputStream fis = new FileInputStream("src/fr/ceri/data/unlabeled.xml");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+
+        JSONObject myJsonObj = XML.toJSONObject(reader, true);
+//        System.out.println();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("src/fr/ceri/data/unlabeled_test_string.json"));
+        writer.write(myJsonObj.toString(4));
+        writer.close();
+
+//        try
+//        {
+//            System.out.println("Tweets");
+//
+//            LexiqueCreator lexLuthor = new LexiqueCreator();
+//            lexLuthor.writeLexiqueToFile();
+//            lexLuthor.transformTweets(new File("src/fr/ceri/data/task1-train.csv"));
+//        }
+//        catch (Exception ex)
+//        {
+//            System.out.println("Exception caught:" + ex.getMessage());
+//            System.out.println("Stack trace:" + Arrays.toString(ex.getStackTrace()));
+//        }
     }
 }
