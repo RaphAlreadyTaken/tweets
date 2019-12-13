5ecdbda7745533cf4009f0cfe232505813a8e978
Loic MENGUY <loicm97@gmail.com>
2019-12-13 02:44:06 +0100

    Premiere version d'annotation automatique: prise en compte des hashtags + mots sortie en fichier json
    TODO: charger vraies listes de données

--- python/CorpusAnnotation.py
+++ python/CorpusAnnotation.py
@@ -1,34 +1,66 @@
 import util
-import re
+import json
 
-# 0: neutre
-# 1: positif
-# 2: négatif
-# 3: mixte
-
-dict_hashtags = {'#irrespect': 2}
-dict_words = {'pute': 2}
+dict_hashtags = {'#irrespect': "negatif", "#2017LeDebat": "neutre", "#Joie": "positif"}
+dict_words = {'pute': "negatif", "content": "positif"}
 dict_date = {}
 
+dict_correspondances = {
+    "positif": 1,
+    "negatif": -1,
+    "neutre": 0
+}
+
 data = util.get_all_tweets()
+
+tweets_polarity = {}
+
 for tweet in data:
     # print(tweet['_source']['message'])
     tweet_score = 0
+    tweet_polarity = "neutre"
+
+    ########### Message ###########
+    isChanged = False
 
     message = tweet['_source']['message']
     message_clean = util.clean_message(message)
     message_clean_splitted = message_clean.split()
 
     for mot in message_clean_splitted:
-        if dict_words.get(mot) == 0:
-            print("coucou")
+        if dict_words.get(mot) is not None:
+            poids = int(dict_correspondances.get(dict_words.get(mot)))
+            tweet_score += poids
+            if poids is not 0:
+                isChanged = True
 
+    ########### Hashtags ###########
+    hashtags = tweet['_source']['hashtags']
+    for curHashtagDict in hashtags:
+        if curHashtagDict is not False:
+            currentHashtag = curHashtagDict.get("text")
+            if dict_hashtags.get(currentHashtag) is not None:
+                poids = int(dict_correspondances.get(dict_hashtags.get(currentHashtag)))
+                tweet_score += poids
+                if poids is not 0:
+                    isChanged = True
 
-    # print(message_clean_splitted_no)
-    # print(message_clean)
-    # for mot in message_clean_splitted_no:
-        # print(mot)
-    # for mot in message_split:
-    #     print(mot)
-    # print("nouveau tweet")
-    # if(tweet)
+    # if tweet_score is not 0:
+    #     print(tweet_score)
+
+    # Sauvegarde du score final du tweet
+    if isChanged is False:
+        tweet_polarity = "neutre"
+    elif tweet_score > 0:
+        tweet_polarity = "positif"
+    elif tweet_score < 0:
+        tweet_polarity = "negatif"
+    else:
+        tweet_polarity = "mixte"
+
+    tweets_polarity[tweet['_id']] = tweet_polarity
+
+print(tweets_polarity)
+
+with open('result.json', 'w') as file:
+    json.dump(tweets_polarity, file)
